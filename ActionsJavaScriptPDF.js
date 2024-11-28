22b14a61d5c25ca9313564a5d8b7fef1df97a874
Francisco Coelho <coelho1@ua.pt>
2024-11-28 11:49:51 +0000

    Added JS for adding materials

--- ActionsJavaScriptPDF.js
+++ ActionsJavaScriptPDF.js
@@ -34,3 +34,27 @@ this.getField("totalMaterial2").value = total;
 
 /* ======================================== */
 
+
+// Get values from the received and used fields for Material 3
+var received = this.getField("receivedMaterial3").valueAsNumber || 0;
+var used = this.getField("usedMaterial3").valueAsNumber || 0;
+
+// Calculate the total for Material 3
+var total = received - used;
+
+//Update the total field for Material 3
+this.getField("totalMaterial3").value = total;
+
+
+/* ======================================== */
+
+
+// Get values from the received and used fields for Material 4
+var received = this.getField("receivedMaterial4").valueAsNumber || 0;
+var used = this.getField("usedMaterial4").valueAsNumber || 0;
+
+// Calculate the total for Material 4
+var total = received - used;
+
+//Update the total field for Material 4
+this.getField("totalMaterial4").value = total;
